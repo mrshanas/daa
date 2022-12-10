7b491553d79dad0ad1677754e18e61b35462d922
Shanas <nassibshaban345@gmail.com>
2022-12-11 00:26:23 +0300

    Starting linked lists

--- data_structures/README.md
+++ data_structures/README.md
@@ -1,1 +1,3 @@
 # Data Structures
+
+A folder about data structures
---
+++ data_structures/linked_lists/List.java
@@ -0,0 +1,7 @@
+package data_structures.linked_lists;
+
+public class List {
+    public static void main(String[] args) {
+        System.out.println("Linked List");
+    }
+}
